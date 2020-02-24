8b3a8afd7be03f904e0b8791608554d193da380d
Stacey Strovink <staceystrovink@gmail.com>
2020-02-23 19:48:04 -0800

    add JS and styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
-  $("form#suggester").submit(fucntion () {
+  $("form#suggester").submit(function() {
     event.preventDefault();
-    var 
-  })
-})
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "JavaScript") {
+      result = $("#JavaScript").fadeToggle();
+    } else if (operator === "Swift") {
+      result = $("#Swift").fadeToggle();
+    } else if (operator === "Python") {
+      result = $("#Python").fadeToggle();
+    }
+    $("#output").text(result);
+  });
+});
